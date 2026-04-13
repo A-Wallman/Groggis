e775c5ee6e2b62dd333be3e4c999b1cae10b6158
Adam W <adamwallman434@gmail.com>
2026-04-13 12:34:23 +0200

    setup för rotary encoders

--- Grogg/src/main.cpp
+++ Grogg/src/main.cpp
@@ -1,20 +1,23 @@
 #include <Arduino.h>
 
+#define rotCLK1 DD2
+#define rotDT1 DD3
+#define rotSW1 DD4
+
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(rotCLK1, INPUT);
+  pinMode(rotDT1, INPUT);
+  pinMode(rotSW1, INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //hej hej hejsan
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making
-  delay(1000);                       // wait for a second
+  
+ 
 }
 
 // put function definitions here:
