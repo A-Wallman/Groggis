3713df2184c6909018788c11ddfcb01f6b1fe310
Adam W <adamwallman434@gmail.com>
2026-04-14 10:57:20 +0200

    display test 1

---
+++ grogg_code/src/main.cpp
@@ -0,0 +1,45 @@
+#include <Arduino.h>
+
+#define rotCLK1 2
+#define rotDT1 3
+#define rotSW1 4
+
+int counter = 0;
+int lastStateCLK = HIGH;
+int currentStateCLK;
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(rotCLK1, INPUT);
+  pinMode(rotDT1, INPUT);
+  pinMode(rotSW1, INPUT_PULLUP);
+  Serial.begin(9600);
+  Serial.println("Setup complete");
+}
+
+void loop() {
+  currentStateCLK = digitalRead(rotCLK1);
+  if (currentStateCLK != lastStateCLK) {
+    if (digitalRead(rotDT1) != currentStateCLK) {
+      //MEDURS ROTATION
+      counter++;
+    } else {
+      //MOTURS ROTATION
+      counter--;
+    }
+    Serial.print("Counter: ");
+    Serial.println(counter);
+  }
+  lastStateCLK = currentStateCLK;
+  // put your main code here, to run repeatedly:
+  
+ 
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
