0c53af9b1b75336cda4b707b91822575caf11c46
Adam W <adamwallman434@gmail.com>
2026-04-14 12:01:35 +0200

    fixa skruv så det skrivs ut på skärm

--- Grogg/src/main.cpp
+++ Grogg/src/main.cpp
@@ -8,6 +8,9 @@
 #define rotDT1 3
 #define rotSW1 4
 
+#define scrnSDA A5
+#define scrnSCK A4
+
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1
@@ -19,19 +22,31 @@ int currentStateCLK;
 int MAXDRYCK1 = 30; // max 30 cl
 
 // put function declarations here:
-int myFunction(int, int);
+void updateDisplay(int screen, int counter) {
+  display.setCursor(12,28);
+  display.clearDisplay();
+  display.println(String(counter) + " cl");
+  display.display();
+}
+
+void displayPrintCl() {
+  
+}
 
 void setup() {
+  Serial.begin(9600);
   // put your setup code here, to run once:
   pinMode(rotCLK1, INPUT);
   pinMode(rotDT1, INPUT);
   pinMode(rotSW1, INPUT_PULLUP);
 
+
+
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
   display.setTextColor(SSD1306_WHITE);
-  display.setTextSize(1);
-  display.setCursor(4,28);
+  display.setTextSize(4);
+  display.setCursor(16,28);
   display.println(String(counter) + " cl");
   display.display();
 }
@@ -46,16 +61,11 @@ void loop() {
       //MOTURS ROTATION
       counter--;
     }
-    Serial.print("Counter: ");
     Serial.println(counter);
+    updateDisplay(0, counter);
   }
   lastStateCLK = currentStateCLK;
   // put your main code here, to run repeatedly:
   
  
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
